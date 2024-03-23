8592c22d78a23e5138a0e773520b09c3bf6f886e
jameer <jameer.a.mulani@gmail.com>
2024-03-23 17:07:48 +0530

    Added restaurant query in search place api

--- app/src/main/java/com/deserve/restaurantsnearme/data/remote/ApiSpec.kt
+++ app/src/main/java/com/deserve/restaurantsnearme/data/remote/ApiSpec.kt
@@ -3,10 +3,7 @@ package com.deserve.restaurantsnearme.data.remote
 import com.deserve.restaurantsnearme.BuildConfig
 import com.deserve.restaurantsnearme.model.dto.PlacePhotoResponse
 import com.deserve.restaurantsnearme.model.dto.SearchPlacesApiResponse
-import com.deserve.restaurantsnearme.model.entity.Restaurant
 import com.deserve.restaurantsnearme.util.Constants
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,6 +21,7 @@ interface ApiSpec {
         @Header("Authorization") authorization: String = BuildConfig.FSQ_APIKEY,
 //        @Query("open_now") openNow: String = "true",
         @Query("sort") sort: String = "DISTANCE",
+        @Query("query") query : String= "restaurant",
         @Query("ll") location: String
     ): Response<SearchPlacesApiResponse>
 
