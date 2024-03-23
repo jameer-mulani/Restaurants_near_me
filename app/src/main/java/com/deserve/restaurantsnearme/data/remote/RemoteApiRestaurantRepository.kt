62a4c98ed5f8069ecf6c5e20f018d24bab727f8b
jameer <jameer.a.mulani@gmail.com>
2024-03-23 17:01:32 +0530

    Adjusted place image dimensions.

--- app/src/main/java/com/deserve/restaurantsnearme/data/remote/RemoteApiRestaurantRepository.kt
+++ app/src/main/java/com/deserve/restaurantsnearme/data/remote/RemoteApiRestaurantRepository.kt
@@ -1,35 +1,46 @@
 package com.deserve.restaurantsnearme.data.remote
 
+import android.content.res.Resources
 import android.util.Log
-import com.deserve.restaurantsnearme.model.entity.Resource
 import com.deserve.restaurantsnearme.data.RestaurantRepository
 import com.deserve.restaurantsnearme.model.dto.GetRestaurantInput
+import com.deserve.restaurantsnearme.model.entity.Resource
 import com.deserve.restaurantsnearme.model.entity.Restaurant
 
 class RemoteApiRestaurantRepository(private val api: ApiSpec) : RestaurantRepository {
     override suspend fun fetchRestaurant(input: GetRestaurantInput): Resource<List<Restaurant>> {
-        val searchPlaces = api.searchPlaces(location = "${input.location.latitude},${input.location.longitude}")
+        val searchPlaces = api.searchPlaces(location = "47.606,-122.349358")
         return if (searchPlaces.isSuccessful) {
             Log.d("repository", "fetchRestaurant: ${searchPlaces.body()}")
             Resource.success(searchPlaces.body()?.results ?: emptyList())
         } else {
-            Resource.failed(searchPlaces.errorBody()?.toString() ?: "Failed to fetch restaurants from api")
+            Resource.failed(
+                searchPlaces.errorBody()?.toString() ?: "Failed to fetch restaurants from api"
+            )
         }
     }
 
-    override suspend fun fetchRestaurantPhoto(restaurantId: String, width : Int,height : Int): Resource<String> {
+    override suspend fun fetchRestaurantPhoto(
+        restaurantId: String,
+        width: Int,
+        height: Int
+    ): Resource<String> {
         val photoResponse = api.getPhotos(id = restaurantId)
         return when {
             photoResponse.isSuccessful -> {
                 val response = photoResponse.body()
                 Log.d("repository", "fetchRestaurantPhoto: ${response}")
                 if (!response.isNullOrEmpty()) {
-                    val url = "${response[0].prefix}${width}x${height}${response[0].suffix}"
+                    val url =
+                        "${response[0].prefix}${Resources.getSystem().displayMetrics.widthPixels}x${
+                            Resources.getSystem().displayMetrics.heightPixels/4
+                        }${response[0].suffix}"
                     Resource.success(url)
                 } else {
                     Resource.success("")
                 }
             }
+
             else -> {
                 Resource.failed(photoResponse.errorBody()?.toString() ?: "")
             }
